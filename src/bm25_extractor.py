81304c5f2725e33224c770a07e4c2a821876eaec
Rushikesh Ghatage <147006024+Rishi625@users.noreply.github.com>
2024-11-18 02:22:38 -0500

    Update bm25_extractor.py

--- src/bm25_extractor.py
+++ src/bm25_extractor.py
@@ -1,10 +1,10 @@
-import os
+from joblib import Parallel, delayed
 import numpy as np
 from rank_bm25 import BM25Okapi
 from tqdm import tqdm
 from joblib import Parallel, delayed
 from src.utils import save_np_array_to_file
-
+import os
 
 class BM25Extractor:
     def __init__(self, feature_type):
@@ -15,19 +15,13 @@ class BM25Extractor:
         self.feature_dir = f"features_{self.feature_type}"
         os.makedirs(self.feature_dir, exist_ok=True)
         
-
     def fit(self, texts):
         self.corpus = [text.split() for text in texts]  # Tokenize texts
         print(len(self.corpus))
         self.bm25 = BM25Okapi(self.corpus)
         print("BM25 model fitted on corpus")
 
-    def _compute_batch_scores(self, batch_queries):
-        """Helper function to compute scores for a batch of queries."""
-        return np.array([self.bm25.get_scores(query) for query in batch_queries])
-
-    def extract_features_and_save(self, texts, batch_size=4):
-        print(texts)
+    def extract_features_and_save(self, texts, batch_size=16, n_jobs=-1):
         if self.bm25 is None:
             raise ValueError("BM25 model is not fitted")
 
@@ -36,19 +30,22 @@ class BM25Extractor:
         print(queries[0])
         
         # Divide queries into batches
-        batches = [queries[i:i+batch_size] for i in range(0, len(queries), batch_size)]
-        print(batches[0])
+        batches = [
+            (queries[i:i+batch_size], i * batch_size, min((i + 1) * batch_size, len(queries)))
+            for i in range(0, len(queries), batch_size)
+        ]
 
-        # Process batches and save results
-        for i, batch in enumerate(tqdm(batches)):
-            start = i * batch_size
-            end = min((i + 1) * batch_size, len(queries))
+        # Process batches in parallel
+        def process(batch, start, end):
             if not os.path.exists(f"{self.feature_dir}/similarity_matrix_{start}_{end}.npy"):
-                similarity_matrix = np.array(self._compute_batch_scores(batch))
+                similarity_matrix = np.array([self.bm25.get_scores(query) for query in batch])
                 save_np_array_to_file(similarity_matrix, f"{self.feature_dir}/similarity_matrix_{start}_{end}.npy")
                 print(f"Saved similarity matrix for {start} to {end} documents to file")
             else:
                 print(f"File similarity_matrix_{start}_{end}.npy already exists, skipping...")
 
+        Parallel(n_jobs=n_jobs)(
+            delayed(process)(batch, start, end) for batch, start, end in tqdm(batches)
+        )
+
         print("All batches processed and saved.")
-   
